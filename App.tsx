29c1a4bac3461b6dffe71fce581241f5595cd8dd
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-26 12:24:48 -0300

    Criando Interface NewGroup

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 
 import { Loading } from '@components/Loading'
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
-import { Groups } from '@screens/Groups'
+import { NewGroup } from '@screens/NewGroup'
 
 import theme from './src/theme'
 
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
