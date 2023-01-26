d8af7c67d4b7939e559aea06c3217c41e1997d1c
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-01-26 14:51:57 -0300

    Criando componente Button Icon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacityProps } from 'react-native'
+
+import { Container, Icon } from './styled'
+
+type Props = TouchableOpacityProps & {};
+
+export const ButtonIcon = ({}: Props) => {
+  return (
+    <Container>
+      <Icon name="" />
+    </Container>
+  );
+};
